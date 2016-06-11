38f6576a741613a9499aad3d452d179995ba6aeb
David Skrenta <davidskrenta@Davids-MacBook-Pro-2.local>
2016-06-10 17:02:14 -0700

    partial completion of tripic function

--- resize.js
+++ resize.js
@@ -1,3 +1,88 @@
+function tripic(images, finalWidth, finalHeight) {
+  var finalCanvasWidth = finalWidth || 810; // default 810 px width
+  var finalCanvasHeight = finalHeight || 415; // default 415 px height
+  var cells = images.length;
+  var sectionOfWidth = (finalCanvasWidth / cells);
+
+  // create final canvas
+  var finalCanvas = document.createElement("canvas");
+  //finalCanvas.setAttribute("type", "hidden");
+  finalCanvas.setAttribute("id", "finalCanvas");
+  finalCanvas.setAttribute("width", finalCanvasWidth);
+  finalCanvas.setAttribute("height", finalCanvasWidth);
+  var finalCtx = finalCanvas.getContext('2d');
+
+  // create image and sub canvas for each image
+  for (var i = 0; i < images.length; i++) {
+    var canvas = document.createElement("canvas"); // create sub canvas
+    //canvas.setAttribute("type", "hidden"); // hide subcanvas element
+    canvas.setAttribute("width", sectionOfWidth); // set subcanvas width to section of final canvas
+    canvas.setAttribute("height", finalCanvasHeight); // set subcanvas height to final canvas height
+    var canvasID = "canvas" + i;
+    canvas.setAttribute("id", canvasID);
+    document.body.appendChild(canvas);
+    var ctx = canvas.getContext('2d'); // get sub canvas context
+    var img = new Image(); // create new image
+    var imageID = "image" + i;
+    img.style.display = "none";
+    img.id = imageID;
+    document.body.appendChild(img);
+    generateOnload(imageID, canvasID);
+    /*
+    img.onload = function() {
+      drawImageProp(ctx, img, 0, 0, canvas.width, canvas.height, 0.5, 0.0);
+      //drawFinal(0);
+      var xPos = 0;
+      if (i != 0) xPos = (sectionOfWidth * i);
+      finalCtx.drawImage(canvas, xPos, 0, canvas.width, canvas.height);
+    }
+    */
+    img.src = images[i];
+  }
+}
+
+function generateOnload(targetImageID, targetCanvasID) {
+  var img = document.getElementById(targetImageID);
+  var canvas = document.getElementById(targetCanvasID);
+  var ctx = canvas.getContext('2d');
+  img.onload = function() {
+    drawImageProp(ctx, img, 0, 0, canvas.width, canvas.height, 0.5, 0.0);
+    console.log(targetImageID + " Loaded");
+  }
+}
+
+function createPic(imgs) {
+  /*
+  var finalCanvas = document.createElement('canvas');
+  finalCanvas.setAttribute('height', 810);
+  finalCanvas.setAttribute('width', 415);
+  var finalCtx = finalCanvas.getContext('2d');
+  */
+
+  var finalCanvasWidth = 810; // default 810 px width
+  var finalCanvasHeight = 415; // default 415 px height
+  var cells = imgs.length;
+  var sectionOfWidth = (finalCanvasWidth / cells);
+
+  for (var i = 0; i < imgs.length; i++) {
+    var canvas = document.createElement("canvas"); // create sub canvas
+    canvas.setAttribute("width", sectionOfWidth); // set subcanvas width to section of final canvas
+    canvas.setAttribute("height", finalCanvasHeight); // set subcanvas height to final canvas height
+    var canvasID = "canvas" + i;
+    canvas.setAttribute("id", canvasID);
+    document.body.appendChild(canvas);
+    var ctx = canvas.getContext('2d'); // get sub canvas context
+
+    var img = new Image();
+    img.onload = function() {
+      console.log(img.src);
+      console.log(ctx);
+      drawImageProp(ctx, img, 0, 0, sectionOfWidth, finalCanvasHeight, 0.5, 0.0);
+    }
+    img.src =  imgs[i];
+  }
+}
+
 function draw(ctx, img, canvas) {
   drawImageProp(ctx, img, 0, 0, canvas.width, canvas.height, 0.5, 0.0);
 }
