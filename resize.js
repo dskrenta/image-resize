3b797eda5b1c6af77983d83e575d8a458ffc591e
David Skrenta <davidskrenta@Davids-MacBook-Pro-2.local>
2016-06-14 14:46:22 -0700

    refactor and html5 canvas test

--- resize.js
+++ resize.js
@@ -1,56 +1,67 @@
+/*
+  Creates tripic, quadpic, and dualpic
+  @param {array} images - array of image urls
+  @param {number} finalWidth - final image width, defaults to 810 px
+  @param {number} finalHeight - final image height, defaults to 415 px
+ */
 function tripic(images, finalWidth, finalHeight) {
-  var finalCanvasWidth = finalWidth || 810; // default 810 px width
-  var finalCanvasHeight = finalHeight || 415; // default 415 px height
-  var cells = images.length;
-  var sectionOfWidth = (finalCanvasWidth / cells);
-
-  // switch for default x and y offsets
-  var offsetX, offsetY;
-  switch (images.length) {
-    case 2: // 2 images
-      offsetX = 0.5;
-      offsetY = 0.5;
-      console.log("2");
-      break;
-    case 3: // 3 images
-      offsetX = 0.5;
-      offsetY = 0.0;
-      console.log("3");
-      break;
-    case 4: // 4 images
-      offsetX = 0.5;
-      offsetY = 0.0;
-      console.log("4");
-      break;
+  var canvasTest = document.createElement('canvas');
+  if (!canvasTest.getContext) {
+    alert('HTML5 canvas is not supported by your browser, please switch to another browser or a newer version');
+    return;
   }
+  else {
+    var finalCanvasWidth = finalWidth || 810; // default 810 px width (FB)
+    var finalCanvasHeight = finalHeight || 415; // default 415 px height (FB)
+    var cells = images.length;
+    var sectionOfWidth = (finalCanvasWidth / cells);
 
-  // create final canvas
-  var finalCanvas = document.createElement("canvas");
-  finalCanvas.setAttribute("id", "finalCanvas");
-  finalCanvas.setAttribute("width", finalCanvasWidth);
-  finalCanvas.setAttribute("height", finalCanvasHeight);
-  document.body.appendChild(finalCanvas);
+    // switch for default x and y offsets
+    var offsetX, offsetY;
+    switch (images.length) {
+      case 2: // 2 images
+        offsetX = 0.5;
+        offsetY = 0.5;
+        break;
+      case 3: // 3 images (recommended for correct size)
+        offsetX = 0.5;
+        offsetY = 0.0;
+        break;
+      case 4: // 4 images
+        offsetX = 0.5;
+        offsetY = 0.0;
+        break;
+    }
 
-  // create image and sub canvas for each image
-  for (var i = 0; i < images.length; i++) {
-    var canvas = document.createElement("canvas"); // create sub canvas
-    canvas.setAttribute("width", sectionOfWidth);
-    canvas.setAttribute("height", finalCanvasHeight); 
-    var canvasID = "canvas" + i;
-    canvas.setAttribute("id", canvasID);
-    canvas.style.display = "none";
-    document.body.appendChild(canvas);
-    var img = new Image(); // create new image
-    var imageID = "image" + i;
-    img.style.display = "none";
-    img.id = imageID;
-    document.body.appendChild(img);
-    generateOnload(imageID, canvasID, "finalCanvas", sectionOfWidth, i, offsetX, offsetY);
-    img.src = images[i];
+    // create final canvas
+    var finalCanvas = document.createElement('canvas');
+    finalCanvas.setAttribute('id', 'finalCanvas');
+    finalCanvas.setAttribute('width', finalCanvasWidth);
+    finalCanvas.setAttribute('height', finalCanvasHeight);
+    document.body.appendChild(finalCanvas);
+
+    // create image and sub canvas for each image
+    for (var i = 0; i < images.length; i++) {
+      var canvas = document.createElement('canvas'); // create sub canvas
+      canvas.setAttribute('width', sectionOfWidth);
+      canvas.setAttribute('height', finalCanvasHeight);
+      var canvasID = 'canvas' + i;
+      canvas.setAttribute('id', canvasID);
+      canvas.style.display = 'none';
+      document.body.appendChild(canvas);
+      var img = new Image(); // create new image
+      var imageID = 'image' + i;
+      img.style.display = 'none';
+      img.id = imageID;
+      document.body.appendChild(img);
+      generateOnload(imageID, canvasID, 'finalCanvas', sectionOfWidth, i, offsetX, offsetY, cells);
+      img.src = images[i];
+    }
   }
 }
 
-function generateOnload(targetImageID, targetCanvasID, finalCanvasID, sectionOfWidth, index, offsetX, offsetY) {
+// generate onload events for images to be redrawn onto canvas with custom offsets
+function generateOnload(targetImageID, targetCanvasID, finalCanvasID, sectionOfWidth, index, offsetX, offsetY, cells) {
   var img = document.getElementById(targetImageID);
   var canvas = document.getElementById(targetCanvasID);
   var ctx = canvas.getContext('2d');
@@ -61,9 +72,22 @@ function generateOnload(targetImageID, targetCanvasID, finalCanvasID, sectionOfW
     var xPos = 0;
     if (index != 0) xPos = (sectionOfWidth * index);
     finalCtx.drawImage(canvas, xPos, 0, canvas.width, canvas.height);
+    if (index === (cells - 1)) {
+      try {
+        var finalImageURL = canvas.toDataURL('image/png');
+        var finalImage = document.createElement('img');
+        //finalImage.style.display = 'none';
+        finalImage.src = finalImageURL;
+        document.body.appendChild(finalImage);
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
   }
 }
 
+// calculate correct offsets to preserve aspect ratio by cropping and draw on temp canvas
 function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
   if (arguments.length === 2) {
     x = y = 0;
